0f14d41d056e47adc5ebd5918eec87366f0f3f38
unknown <anikmitraxd@gmail.com>
2025-08-14 03:36:09 +0600

    New beta 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['@clerk/nextjs'],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
-  output: 'standalone',
-};
+const nextConfig = {};
 
 export default nextConfig;
